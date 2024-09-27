1a6a92e220f5ce0a30f6a0c7df5e892cd9fc4b1b
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-09-27 02:00:40 +0100

    feat: add jest

--- @types/express.d.ts
+++
@@ -1,8 +0,0 @@
-// express.d.ts
-import 'express';
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    userId?: string; // Optional userId
-  }
-}
---
+++ src/__test__/sum.test.ts
@@ -0,0 +1,5 @@
+import sum from './sum';
+
+const result: number = sum(3, 5);
+
+console.log(result);
