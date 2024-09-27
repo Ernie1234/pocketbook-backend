a39ea0421c7e33d46d74ff71f02058c89df20646
Ernie1234 <anumahjoshuaeneye@gmail.com>
2024-09-27 02:43:04 +0100

    fix: mailtrap token error

--- src/mails/mailtrap.ts
+++ src/mails/mailtrap.ts
@@ -6,7 +6,8 @@ dotenv.config();
 const TOKEN = process.env.MAILTRAP_TOKEN as string;
 
 if (!TOKEN) {
-  throw new Error('Mailtrap token is missing in environment variables');
+  console.error('Mailtrap token is missing in environment variables');
+  process.exit(1); // Exit the process with a failure code
 }
 
 // Initialize Mailtrap client
